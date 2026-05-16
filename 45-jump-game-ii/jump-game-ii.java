b9d6856d48b4f21a9632f7ad5acc353fe721ffd6
Mohanapriya Chandrasekaran <priyanandhinic2005@gmail.com>
2026-05-16 11:32:42 +0530

    Time: 1 ms (99.59%) | Memory: 47.7 MB (8.25%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int jump(int[] nums) {
+
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        // No need to process last index
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            // Find farthest reachable index
+            farthest = Math.max(farthest, i + nums[i]);
+
+            // End of current jump range
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+}
