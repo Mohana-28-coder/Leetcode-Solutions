f20724b82d68e48e6a566f254ed708f75bab8738
Mohanapriya Chandrasekaran <priyanandhinic2005@gmail.com>
2026-05-18 18:54:54 +0530

    Time: 50 ms (80.91%) | Memory: 52.5 MB (23.70%) - LeetSync

---
+++ 384-shuffle-an-array/shuffle-an-array.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    private int[] original;
+    private int[] array;
+    private Random rand;
+
+    public Solution(int[] nums) {
+        original = nums.clone();
+        array = nums.clone();
+        rand = new Random();
+    }
+
+    public int[] reset() {
+        array = original.clone();
+        return array;
+    }
+
+    public int[] shuffle() {
+
+        int[] shuffled = array.clone();
+
+        // Fisher-Yates Shuffle
+        for (int i = shuffled.length - 1; i > 0; i--) {
+
+            int j = rand.nextInt(i + 1);
+
+            // Swap
+            int temp = shuffled[i];
+            shuffled[i] = shuffled[j];
+            shuffled[j] = temp;
+        }
+
+        return shuffled;
+    }
+}
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
